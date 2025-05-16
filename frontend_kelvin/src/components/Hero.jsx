5372b63a877bc22b5f14e95e39c94c3444b2822a
Sulthan000 <syedsulthanbasha@srnritsolutions.com>
2025-05-16 12:39:36 +0530

    Module changes

--- frontend_kelvin/src/components/Hero.jsx
+++ frontend_kelvin/src/components/Hero.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import Logo from '../assets/navlogo.svg';
-import '../css/Hero.css';
+import HeroStyles from '../css/Hero.module.css';
 
 const Hero = () => {
     return (
-        <div className="main-section">
-            <div className="hero-overlay">
-                <Container fluid className="hero-content-container">
-                    <div className="hero-logo ">
+        <div className={HeroStyles.mainSection}>
+            <div className={HeroStyles.heroOverlay}>
+                <Container fluid className={HeroStyles.heroContentContainer}>
+                    <div className={HeroStyles.heroLogo}>
                         <img src={Logo} width={150} />
                     </div>
-                    <div className="hero-text text-center">
-                        <div className="organic-text">ORGANIC</div>
-                        <div className="vegetable-text">VEGETABLE & FRUITS</div>
-                        <div className="subtitle-text">
+                    <div className={` ${HeroStyles.heroText} text-center`}>
+                        <div className={HeroStyles.organicText}>ORGANIC</div>
+                        <div className={HeroStyles.vegetableText}>VEGETABLE & FRUITS</div>
+                        <div className={HeroStyles.subtitleText}>
                             "Experience the Taste of Real Organic"
                         </div>
                     </div>
